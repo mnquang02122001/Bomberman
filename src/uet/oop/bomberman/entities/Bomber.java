e827e729592fa77b39416ecd5801d501dc728199
unknown <hung182301@gmail.com>
2020-11-19 08:04:43 +0700

    hmmm

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -20,6 +20,7 @@ public class Bomber extends Entity {
                 sprite = Sprite.movingSprite(Sprite.player_up_1, Sprite.player_up_2, animate, 20);
 
             }
+            setMoving(0);
         } else if (direction == 1) {
             sprite = Sprite.player_down;
             if (moving==1&&check[upper(yUnit)*width+(int)xUnit]==0) {
@@ -27,6 +28,7 @@ public class Bomber extends Entity {
                 updateLocation();
                 sprite = Sprite.movingSprite(Sprite.player_down_1, Sprite.player_down_2, animate, 20);
             }
+            setMoving(1);
         } else if (direction == 2&&check[(int)yUnit*width+down(xUnit)]==0) {
             sprite = Sprite.player_left;
             if (moving==2&&check[(int)yUnit*width+down(xUnit)]==0) {
@@ -34,6 +36,7 @@ public class Bomber extends Entity {
                 updateLocation();
                 sprite = Sprite.movingSprite(Sprite.player_left_1, Sprite.player_left_2, animate, 20);
             }
+            setMoving(2);
         } else if (direction == 3) {
             sprite = Sprite.player_right;
             if (moving==3&&check[(int)yUnit*width+upper(xUnit)]==0) {
@@ -41,6 +44,7 @@ public class Bomber extends Entity {
                 updateLocation();
                 sprite = Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, animate, 20);
             }
+            setMoving(3);
         } else {
             sprite = Sprite.player_right;
         }
