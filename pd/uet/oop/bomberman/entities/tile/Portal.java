2b27cef3bd838cf3e6d9aad1c59c2b8a28807954
Adriannguyen <phuducnguyen108@gmail.com>
2020-12-02 21:27:35 +0700

    pd
    pd

---
+++ pd/uet/oop/bomberman/entities/tile/Portal.java
@@ -0,0 +1,32 @@
+package uet.oop.bomberman.entities.tile;
+
+import uet.oop.bomberman.Board;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.bomb.Bomb;
+import uet.oop.bomberman.entities.character.Bomber;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Portal extends Tile {
+	private Board _board;
+	public Portal(int x, int y, Sprite sprite,Board board) {
+		super(x, y, sprite);
+		_board = board;
+	}
+	
+	@Override
+	public boolean collide(Entity e) {
+		// TODO: xử lý khi Bomber đi vào
+		if (e instanceof Bomber)
+		{
+			if (_board.detectNoEnemies()==false) return false;
+			if (_board.detectNoEnemies() && e.getXTile() == getX() && e.getYTile() == getY())
+			{
+				_board.nextLevel();
+
+			}
+			return true;
+		}
+		return false;
+	}
+
+}
